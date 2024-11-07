30fdbbb91324fc0a76c4fd9e5e06440e7738f903
minsung <minsung21c@gmail.com>
2024-11-07 11:44:41 +0900

    inner join 테스팅 추가

--- hr_1104.sql
+++ hr_1104.sql
@@ -100,4 +100,30 @@ select first_name, salary, commission_pct, salary+(salary*commission_pct) as tot
 select first_name, salary, commission_pct, salary+(salary*NVL(commission_pct,0)) as total from employees;
 select first_name, salary, commission_pct, salary+(salary*NVL2(commission_pct, commission_pct, 0)) as total from employees;
 
+-- 부서별에 따라 급여를 인상하도록 하자.(조인: inner join, outer join, self join, cross join)
+-- (직원번호, 직원명, 직급, 급여)
+-- 부서명이 'Marketing'인 직원은 5%, 'Purchasing'인 사원은 10%,
+-- 'Human Resources'인 사원은 15%, 'IT'인 직원은 20% 인상한다.
+select * from departments;
+select employees _id, first_name, salary, department_id from employees;
+select * from employees inner join departments on employees.department_id = departments.department_id;
 
+select employee_id, first_name, job_id, salary, E.department_id, D.department_name,
+    case
+    when upper(D.department_name) = upper('Marketing') then salary * 1.05
+    when upper(D.department_name) = upper('Purchasing') then salary * 1.10
+    when upper(D.department_name) = upper('Human Resources') then salary * 1.15
+    when upper(D.department_name) = upper('IT') then salary * 1.20
+    end NEWSALARY
+from employees E inner join departments D on E.department_id = D.department_id
+where upper(D.department_name) in (upper('Marketing'), upper('Purchasing'), upper('Human Resources'), upper('IT'))
+order by NEWSALARY DESC;
+
+-- 지연
+select employee_id, first_name, E.department_id, salary,
+        decode(upper(D.department_name), upper('Marketing'), salary * 1.05, upper('Purchasing'), salary * 1.1, 
+        upper('Human Resources'), salary * 1.15, upper('IT'), salary * 1.2) as salary2
+from departments D inner join employees E on E.department_id = D.department_id
+where upper(D.department_name) in (upper('Marketing'), upper('Purchasing'), upper('Human Resources'), upper('IT'))
+order by salary2 DESC;
+-- inner join, left outer join, right outer join
