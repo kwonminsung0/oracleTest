ff5a92c82a0e0380db1fbea0222dea122837dbeb
minsung <minsung21c@gmail.com>
2024-11-15 10:16:32 +0900

    PL/SQL FOR IN LOOP 구구단 테스팅

--- HR_1115_PLSQL.sql
+++ HR_1115_PLSQL.sql
@@ -1,11 +1,17 @@
 -- PL/SQL
--- departments 에서 반복문을 이용해서 부서 10, 20 ,30 ,40 정보를 출력하시오.
+-- FOR IN LOOP 구구단 작성하기
 DECLARE
-    VDEPARTMENTS DEPARTMENTS%ROWTYPE;
+    VDAN NUMBER;
+    VNUM NUMBER;
 BEGIN
-    FOR I IN REVERSE 1..9 LOOP
-        SELECT * INTO VDEPARTMENTS FROM DEPARTMENTS WHERE DEPARTMENT_ID = I*10;
-        DBMS_OUTPUT.PUT_LINE(VDEPARTMENTS.DEPARTMENT_ID || ' / ' || VDEPARTMENTS.DEPARTMENT_NAME);
+    FOR I IN 1..9 LOOP
+        VDAN := I;
+        DBMS_OUTPUT.PUT_LINE('*****구구단 '||VDAN||'단*****');
+        FOR I IN 1..9 LOOP
+            VNUM := I;
+            DBMS_OUTPUT.PUT_LINE(VDAN || ' * ' || VNUM || ' = ' || VDAN*VNUM);
+        END LOOP;
+        DBMS_OUTPUT.PUT_LINE('');
     END LOOP;
 END;
 /
