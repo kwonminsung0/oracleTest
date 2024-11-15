2e1d9c4fed3a1940328435e22279af15d732620c
minsung <minsung21c@gmail.com>
2024-11-15 09:23:18 +0900

    PL/SQL 구구단 출력테스팅

--- HR_1114_PLSQL.sql
+++ HR_1114_PLSQL.sql
@@ -1,34 +1,27 @@
 -- PL/SQL
--- EMPLOYEE 테이블에서 임의의 부서번호를 랜덤으로 생성한 뒤, 해당된 부서번호 최고연봉을 출력한뒤, 평가하여라(낮음, 높음, 중간, 최고, 없음)
+-- 구구단 작성하기
 DECLARE
-    -- 부서번호, 최고연봉선언
-    VNO NUMBER(4);
-    VTOP_SALARY NUMBER(12,2);
-    VRESULT VARCHAR2(20);
+    VNUM NUMBER := 3;
+    VCOUNT NUMBER := 1;
+    VDAN NUMBER := 0;
 BEGIN
-    -- 임의의 부서번호 생성하기 (RANDOM)
-    VNO := ROUND(DBMS_RANDOM.VALUE(10,110),-1);
-    
-    SELECT SALARY INTO VTOP_SALARY
-    FROM (SELECT SALARY FROM EMPLOYEES WHERE DEPARTMENT_ID = VNO ORDER BY SALARY DESC)
-    WHERE ROWNUM <= 1;
-    -- 평가내리기 1~5000 낮음, 5000~10000 중간, 10000~20000 높음, 20000~ 최고, 없으면 예외처리
-    IF(VTOP_SALARY BETWEEN 1 AND 5000)THEN
-        VRESULT := '낮음';
-    ELSIF(VTOP_SALARY BETWEEN 5000 AND 10000)THEN
-        VRESULT := '중간';
-    ELSIF(VTOP_SALARY BETWEEN 10000 AND 20000)THEN
-        VRESULT := '높음';
-    ELSE
-        VRESULT := '최고';
-    END IF;
-    
-    DBMS_OUTPUT.PUT_LINE('부서번호: ' || VNO);
-    DBMS_OUTPUT.PUT_LINE('최고연봉: ' || VTOP_SALARY);
-    DBMS_OUTPUT.PUT_LINE('최고연봉평가: ' || VRESULT);
-    
-EXCEPTION
-    WHEN NO_DATA_FOUND THEN
-        DBMS_OUTPUT.PUT_LINE(VNO || '해당부서에 해당되는 사원이 없습니다. ');
+    LOOP
+        VDAN := VDAN + 1;
+        VCOUNT := 1;
+        DBMS_OUTPUT.PUT_LINE('*****구구단 '||VDAN||'단*****');
+        -- VDAN 단을 출력
+        LOOP
+            DBMS_OUTPUT.PUT_LINE(VDAN || ' * ' || VCOUNT || ' = ' || VDAN*VCOUNT);
+            VCOUNT := VCOUNT + 1;
+            IF VCOUNT > 9 THEN
+                EXIT;
+            END IF;
+        END LOOP;
+        DBMS_OUTPUT.PUT_LINE('');
+        -- 단 출력
+        IF VDAN > 9 THEN
+            EXIT;
+        END IF;
+    END LOOP;
 END;
 /
