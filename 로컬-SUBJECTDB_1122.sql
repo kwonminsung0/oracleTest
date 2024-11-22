9f90b579410e2a774fc16ee71800a6a73bcacea7
minsung <minsung21c@gmail.com>
2024-11-22 12:45:00 +0900

    SUBJECTDB 생성(테이블 생성, 제약조건 생성, 시퀀스 생성)

---
+++ 로컬-SUBJECTDB_1122.sql
@@ -0,0 +1,68 @@
+-- 학과 (01-컴퓨터학과 / 02-교육학과 / 03-신문방송학과 / 04-인터넷비즈니스과 / 05-기술경영과)
+create table subject(
+    no number,                  -- pk, seq
+    num varchar2(2) not null,   -- 학과번호 01, 02, 03, 04, 05
+    name varchar2(24) not null  -- 학과이름
+);
+ALTER table subject add constraint subject_no_pk PRIMARY KEY(no);
+ALTER table subject add constraint subject_num_uk UNIQUE(num);
+
+create sequence subject_seq
+start with 1
+increment by 1;
+
+-- 학생
+create table student( 
+    no number,                     -- PK, SEQ
+    num varchar2(8) not null,      -- 학번(년도,학과번호)
+    name varchar2(12) not null,    -- 이름
+    id varchar2(12) not null,      -- 아이디
+    passwd varchar2(12) not null,  -- 패스워드
+    s_num varchar2(2) not null,    -- 학과번호(FK)
+    birthday varchar2(8) not null, -- 생년월일
+    phone varchar2(15) not null,   -- 전화번호
+    address varchar2(80) not null, -- 주소
+    email varchar2(40) not null,   -- 이메일
+    sdate date default sysdate     -- 등록일
+);
+ALTER table student add constraint student_no_pk PRIMARY KEY(no);
+ALTER table student add constraint student_num_uk UNIQUE(num);
+ALTER table student add constraint student_id_uk UNIQUE(id);
+ALTER table student add constraint student_subject_num_fk 
+    FOREIGN KEY(s_num) References subject(num) on delete set null;
+ALTER table student drop constraint student_subject_num_fk;
+
+create sequence student_seq
+start with 1
+increment by 1;
+
+-- lesson 과목
+create table lesson( 
+    no number,                  --PK, SEQ
+    abbre varchar2(2) not null, -- 과목요약
+    name varchar2(20) not null  -- 과목이름
+);
+ALTER table lesson add constraint lesson_no_pk PRIMARY KEY(no);
+ALTER table lesson add constraint lesson_abbre_uk UNIQUE(abbre);
+
+create sequence lesson_seq 
+start with 1
+increment by 1;
+
+-- trainee 수강신청
+create table trainee( 
+    no number ,                    -- PK, SEQ
+    s_num varchar2(8) not null,    -- student(FK) 학생번호
+    abbre varchar2(2) not null,    -- lesson(FK) 과목요약
+    section varchar2(20) not null, -- 전공, 부전공, 교양
+    tdate date default sysdate     -- 수강신청일
+);
+ALTER table trainee add constraint trainee_no_pk PRIMARY KEY(no);
+ALTER table trainee add constraint trainee_student_num_fk 
+    FOREIGN KEY(s_num) References student(num) on delete set null;
+ALTER table trainee add constraint trainee_lesson_abbre_fk 
+    FOREIGN KEY(abbre) References lesson(abbre) on delete set null;
+
+create sequence trainee_seq 
+start with 1
+increment by 1;
