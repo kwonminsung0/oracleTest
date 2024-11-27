d05a2894fd56cdb089e1bce50139637ab0dd0572
minsung <minsung21c@gmail.com>
2024-11-27 16:43:52 +0900

    SUBJECTDB 사용자 계정 생성

--- SYSTEM_사용자생성.sql
+++ SYSTEM_사용자생성.sql
@@ -1,7 +1,15 @@
 -- 사용자 계정만들기 (시스템관리자모드에서 진행해야됨)
 ALTER SESSION SET "_ORACLE_SCRIPT"=true;
-DROP USER HOSPITAL CASCADE; -- 기존 사용자 삭제
-CREATE USER HOSPITAL IDENTIFIED BY 123456 -- 사용자 이름: Model, 비밀번호 : 1234
+DROP USER STUDENTDB CASCADE; -- 기존 사용자 삭제
+CREATE USER STUDENTDB IDENTIFIED BY 123456 -- 사용자 이름: Model, 비밀번호 : 1234
     DEFAULT TABLESPACE USERS  -- 데이터 저장소
     TEMPORARY TABLESPACE TEMP;  -- 임시저장장소
-GRANT connect, resource, dba TO HOSPITAL; -- 권한 부여
+GRANT connect, resource, dba TO STUDENTDB; -- 권한 부여
+
+
+ALTER SESSION SET "_ORACLE_SCRIPT"=true;
+DROP USER SUBJECTDB CASCADE; -- 기존 사용자 삭제
+CREATE USER SUBJECTDB IDENTIFIED BY 123456 -- 사용자 이름: Model, 비밀번호 : 1234
+    DEFAULT TABLESPACE USERS  -- 데이터 저장소
+    TEMPORARY TABLESPACE TEMP;  -- 임시저장장소
+GRANT connect, resource, dba TO SUBJECTDB; -- 권한 부여
