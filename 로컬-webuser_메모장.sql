d5b8c91325456b4df164a850d243d1e388bcb2d6
minsung <minsung21c@gmail.com>
2024-12-03 14:23:41 +0900

    JSP 웹메모장 테이블 테스트

---
+++ 로컬-webuser_메모장.sql
@@ -0,0 +1,14 @@
+CREATE SEQUENCE visit_seq  -- 시퀀스이름
+   START WITH 1             -- 시작을 1로 설정
+   INCREMENT BY 1          -- 증가 값을 1씩 증가
+   NOMAXVALUE             -- 최대 값이 무한대
+   NOCACHE
+   NOCYCLE;
+
+CREATE table VISIT (
+    NO       NUMBER(5,0) NOT NULL,
+    WRITER   VARCHAR2(20) NOT NULL,
+    MEMO     VARCHAR2(4000) NOT NULL,
+    REGDATE  DATE NOT NULL
+);
+ALTER TABLE VISIT ADD CONSTRAINT VISIT_NO_PK PRIMARY KEY(NO);
